7e07cf0cff088121efcbf9a7805d3b4f15a2bc27
lechaudepierre <lechaudepierre@gmail.com>
2026-05-05 09:08:11 +0000

    hey

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,6 +21,7 @@ router.get("/", async (_, res) => {
 router.post("/messages", async (req, res) => {
   // TODO: Implement /messages POST endpoint
   //salut a tous
+  //heyyyy
   res.status(501).end();
 });
 
